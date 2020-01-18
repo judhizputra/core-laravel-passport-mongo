fee3a18f9818f573fdd30e7a2c5e334e06870f73
Yudhistira Eka Putra <yudhistira.eka.putra@gmail.com>
2020-01-19 00:08:40 +0700

    Update UserRepository.php

--- src/Bridge/UserRepository.php
+++ src/Bridge/UserRepository.php
@@ -55,4 +55,33 @@ class UserRepository implements UserRepositoryInterface
 
         return new User($user->getAuthIdentifier());
     }
+
+    /**
+     * Supporting multi auth, useful to separate between users and admins
+     */
+    public function getEntityByUserCredentials($username, $password, $grantType, ClientEntityInterface $clientEntity, $provider)
+    {
+        if (is_null($model = config('auth.providers.'.$provider.'.model'))) {
+            throw new RuntimeException('Unable to determine user model from configuration.');
+        }
+
+        if (method_exists($model, 'findForPassport')) {
+            $user = (new $model)->findForPassport($username);
+        } else {
+            $user = (new $model)->where('email', $username)->first();
+        }
+
+
+        if (! $user ) {
+            return;
+        } elseif (method_exists($user, 'validateForPassportPasswordGrant')) {
+            if (! $user->validateForPassportPasswordGrant($password)) {
+                return;
+            }
+        } elseif (! $this->hasher->check($password, $user->password)) {
+            return;
+        }
+
+        return new User($user->getAuthIdentifier());
+    }
 }
